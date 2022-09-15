1f53ab9d6a88ae8cf53620e310d4f4fe53a0674c
Denis <denverm@yandex.ru>
2022-09-15 20:43:55 +0300

    Add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetArray(int n)
+{
+    string[] startArray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i + 1}-й элемент массива: ");
+        startArray[i] = Console.ReadLine();
+    }
+    return startArray;
+}
+
+int GetLengthNewArray(string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] GetNewArray(string[] array2, int m)
+{
+    string[] mass = new string[m];
+    int count = 0;
+    for (int i = 0; i < array2.Length; i++)
+    {
+        if (array2[i].Length <= 3)
+        {
+            mass[count] = array2[i];
+            count++;
+        }
+    }
+    return mass;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($" ['{String.Join("', '", array)}']");
+}
+
+
+Console.Write("Введите количество элементов массива: ");
+int startLength = int.Parse(Console.ReadLine());
+string[] startArray = GetArray(startLength);
+int newLength = GetLengthNewArray(startArray);
+string[] newArray = GetNewArray(startArray, newLength);
+Console.WriteLine("Введенный массив: ");
+PrintArray(startArray);
+Console.WriteLine("Новый массив: ");
+PrintArray(newArray);
+Console.WriteLine();
